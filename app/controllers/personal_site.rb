46f7fb579ea49d1ab02059e936fe604bce2b38a8
davisc0801 <davisc1014@gmail.com>
2019-04-25 14:15:40 -0600

    Add personal site default response

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -1,3 +1,8 @@
-class PersonalSite
+require "rack"
 
+class PersonalSite
+  def self.call(env)
+    #this is the HTTP response status code, response headers and body
+    ['200', {'Content_Type' => 'text/html'}, ['Welcome!']]
+  end
 end
