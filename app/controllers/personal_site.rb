d99aaae0f93644a116585fa60e71856c8e39d797
davisc0801 <davisc1014@gmail.com>
2019-04-25 14:37:23 -0600

    Add an index html document and view folder for storage.

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -3,6 +3,6 @@ require "rack"
 class PersonalSite
   def self.call(env)
     #this is the HTTP response status code, response headers and body
-    ['200', {'Content_Type' => 'text/html'}, ['Welcome!']]
+    ['200', {'Content_Type' => 'text/html'}, [File.read('./app/views/index.html')]]
   end
 end
