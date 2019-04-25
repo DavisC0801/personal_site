b2628cf97d513879da40c439a06177c5ccffb313
davisc0801 <davisc1014@gmail.com>
2019-04-25 15:11:41 -0600

    Add case statement to display error page

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -1,8 +1,21 @@
 require "rack"
 
 class PersonalSite
+
   def self.call(env)
+    case env["PATH_INFO"]
+    when '/' then index
+    else
+      error
+    end
+  end
+
+  def self.index
     #this is the HTTP response status code, response headers and body
     ['200', {'Content_Type' => 'text/html'}, [File.read('./app/views/index.html')]]
+  end
+
+  def self.error
+    ['404', {'Content_Type' => 'text/html'}, [File.read('./app/views/error.html')]]
   end
 end
