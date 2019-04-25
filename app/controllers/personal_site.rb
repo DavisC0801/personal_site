de7dec1ba3ebc84617b66a01f439491fd2551914
davisc0801 <davisc1014@gmail.com>
2019-04-25 16:07:48 -0600

    Add CSS routing to site

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -6,6 +6,7 @@ class PersonalSite
     case env["PATH_INFO"]
     when '/' then index
     when '/about' then about
+    when '/main.css' then css
     else
       error
     end
@@ -23,9 +24,17 @@ class PersonalSite
     serve_page("about")
   end
 
+  def self.css
+    serve_css("main")
+  end
+
   def self.serve_page(path, code = 200)
     #this is the HTTP response status code, response headers and body
     [code, {'Content_Type' => 'text/html'}, [File.read("./app/views/#{path}.html")]]
+  end
+
+  def self.serve_css(path)
+    [200, {'Content_Type' => 'text/html'}, [File.read("./app/public/#{path}.css")]]
   end
 
 end
