910a78dd275289f4895c35703f8d9a65b6040417
davisc0801 <davisc1014@gmail.com>
2019-04-25 11:33:17 -0600

    Add homepage feature test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,9 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'capybara/minitest'
-require "./app/controller/personal_site.rb"
+require "./app/controllers/personal_site.rb"
+
+# instructions for this are in capybara docs, setup section.
 
 Capybara.app = PersonalSite
 
@@ -9,3 +11,10 @@ class CapybaraTestCase < Minitest::Test
   include Capybara::DSL
   include Capybara::Minitest::Assertions
 end
+
+# for use with multiple sessions or drivers
+
+# def teardown
+#   Capybara.reset_sessions!
+#   Capybara.use_default_driver
+# end
